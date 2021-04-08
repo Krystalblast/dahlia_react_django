93a3a69c65e82c03039add035ed2557650606b35
TCross <t05401btv@gmail.com>
2021-04-07 21:26:57 -0400

    Removed ping, not needed anymore

--- backend/project/project/urls.py
+++ backend/project/project/urls.py
@@ -20,6 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('csrf/', views.csrf, name='csrf'),
-    path('ping/', views.ping, name='ping'),
     path('rest-auth/',include('restauth.urls')),
 ]
